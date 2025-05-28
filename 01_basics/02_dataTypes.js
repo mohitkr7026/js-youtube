e88f668482fd30c8f2862c7ddf116ae24219024c
mohitkr7026 <158762925+mohitkr7026@users.noreply.github.com>
2025-05-28 02:46:21 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";  // treat all js code newer version
+
+// alert(3 + 3) we are using node js, not broswer
+
+let name = "Mohit"
+let age = 18
+let isLoggedIn = false
+let state
+
+// number = 2 to power 53
+// bigint 
+// string = ""
+// boolean = true / false
+// null = standalone value
+// undefined 
+// symbol = unique
+
+console.log(typeof age)
