52a928c75b4eed2bf1db34977c8e25b7062c546c
mohitkr7026 <158762925+mohitkr7026@users.noreply.github.com>
2025-05-28 03:12:04 +0000

    converion of datatpye

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33sbc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber)
+
+// "33aba" = Nan
+// true = 1
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
